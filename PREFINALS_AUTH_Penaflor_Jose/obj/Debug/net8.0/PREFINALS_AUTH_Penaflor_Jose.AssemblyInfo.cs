6334272ac39703d3c90607e52b65d5c7045640b7
Peñaflor_Jose_BSCS32E1 <160807078+Penaflor-Jose-BSCS32E1@users.noreply.github.com>
2024-05-03 14:46:18 +0800

    added configuration for authserver

--- PREFINALS_AUTH_Penaflor_Jose/obj/Debug/net8.0/PREFINALS_AUTH_Penaflor_Jose.AssemblyInfo.cs
+++ PREFINALS_AUTH_Penaflor_Jose/obj/Debug/net8.0/PREFINALS_AUTH_Penaflor_Jose.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PREFINALS_AUTH_Penaflor_Jose")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8c26ba473d26fabe2a9deaf00d4c19929bf53d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a198d853d6bc5c59c1863d003a4814ed119abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PREFINALS_AUTH_Penaflor_Jose")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PREFINALS_AUTH_Penaflor_Jose")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
